2966d951463df3d2adf51d1c4c73dec4ad9815b3
Kimberly Banegas <90736377+kimbanegas15@users.noreply.github.com>
2021-10-09 18:16:05 -0600

    Tienda part 2
    Pequeños cambios en el diseño

---
+++ Tienda parte 2/Win.Tienda/FormModelo.cs
@@ -0,0 +1,26 @@
+﻿using BL.Tienda;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Tienda
+{
+    public partial class FormModelo : Form
+    {
+        ModeloBL _modelos;
+
+        public FormModelo()
+        {
+            InitializeComponent();
+
+            _modelos = new ModeloBL();
+            listaModelosBindingSource.DataSource = _modelos.ObtenerModelos();
+        }
+    }
+}
