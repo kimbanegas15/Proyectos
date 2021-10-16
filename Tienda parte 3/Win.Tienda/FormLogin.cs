165445d361b1660c2497f58c16ffd1b8e2be0132
Kimberly Banegas <90736377+kimbanegas15@users.noreply.github.com>
2021-10-15 20:05:28 -0600

    Formulario de Mantenimiento
    Parte 3 del proyecto de Tienda Vinil

---
+++ Tienda parte 3/Win.Tienda/FormLogin.cs
@@ -0,0 +1,61 @@
+﻿using BL.Tienda;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Tienda
+{
+    public partial class FormLogin : Form
+    {
+        SeguridadBL _seguridad;
+
+        public FormLogin()
+        {
+            InitializeComponent();
+
+            _seguridad = new SeguridadBL();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario;
+            string contrasena;
+
+            usuario = textBox1.Text;
+            contrasena = textBox2.Text;
+
+            var resultado = _seguridad.Autorizar(usuario, contrasena);
+
+
+            if (resultado == true)
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta");
+            }            
+        }
+    }
+}
